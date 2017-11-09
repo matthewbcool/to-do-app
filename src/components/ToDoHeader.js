422f28df0b1fcf1cfb33a6c65f9e33e6aaff0746
Matt Cool <matthewbcool@gmail.com>
2017-11-09 17:04:36 -0600

    progress?

--- src/components/ToDoHeader.js
+++ src/components/ToDoHeader.js
@@ -3,9 +3,7 @@ import React from 'react';
 export class ToDoHeader extends React.Component {
 	render () {
 		return (
-	      <div>
-            <p>this is something</p>
-          </div>  
+          <div></div>
 		);
 	}
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ToDoHeader from './ToDoHeader';
 
 class ToDoList extends React.Component {
+    renderItems() {
+      let tasks = this.props.todos;
+      let taskList = [];
+      let i = 0;
+      for (i=0; i < tasks.length; i++){
+        taskList.push(tasks[i].task);
+        };
+      return taskList; 
+    }
+
+
 	render () {
 		return (
-	      <div>
-             <table>
+             <li>
                <ToDoHeader />
-            </table>
-          </div>  
+               		{this.renderItems()}
+            </li> 
 		);
 	}
 }
